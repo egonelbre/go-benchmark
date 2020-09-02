5bbeb015ef3c8180e140cf8707766f4ed2e56508
Egon Elbre <egonelbre@gmail.com>
2020-09-02 08:25:39 +0300

    Use go modules for dependency tracking.

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/json-iterator/go-benchmark
+
+go 1.15
+
+require (
+	git.apache.org/thrift.git v0.13.0
+	github.com/buger/jsonparser v1.0.0
+	github.com/gogo/protobuf v1.3.1
+	github.com/golang/protobuf v1.4.2
+	github.com/json-iterator/go v1.1.10
+	github.com/mailru/easyjson v0.7.6
+)
---
+++ large_file_test.go
@@ -0,0 +1,94 @@
+package go_benchmark
+
+import (
+	"encoding/json"
+	"flag"
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/buger/jsonparser"
+	jsoniter "github.com/json-iterator/go"
+)
+
+var large = flag.Bool("large", false, "run test with large json file")
+
+func Test_jsonparser_skip(t *testing.T) {
+	if !*large {
+		t.Skip("Large test skipped. Use -large to run.")
+	}
+	file, _ := os.Open("/tmp/large-file.json")
+	bytes, _ := ioutil.ReadAll(file)
+	file.Close()
+	total := 0
+	jsonparser.ArrayEach(bytes, func(value []byte, dataType jsonparser.ValueType, offset int, err error) {
+		total++
+	})
+	if total != 11351 {
+		t.Fatal(total)
+	}
+}
+
+func Test_jsoniter_skip(t *testing.T) {
+	if !*large {
+		t.Skip("Large test skipped. Use -large to run.")
+	}
+	for i := 0; i < 100; i++ {
+		file, _ := os.Open("/tmp/large-file.json")
+		iter := jsoniter.Parse(jsoniter.ConfigDefault, file, 4096)
+		total := 0
+		for iter.ReadArray() {
+			iter.Skip()
+			total++
+		}
+		file.Close()
+		if total != 11351 {
+			t.Fatal(total)
+		}
+	}
+}
+
+func Benchmark_jsonparser(b *testing.B) {
+	if !*large {
+		b.Skip("Large benchmark skipped. Use -large to run.")
+	}
+	b.ReportAllocs()
+	for n := 0; n < b.N; n++ {
+		file, _ := os.Open("/tmp/large-file.json")
+		bytes, _ := ioutil.ReadAll(file)
+		file.Close()
+		total := 0
+		jsonparser.ArrayEach(bytes, func(value []byte, dataType jsonparser.ValueType, offset int, err error) {
+			total++
+		})
+	}
+}
+
+func Benchmark_stardard_lib(b *testing.B) {
+	if !*large {
+		b.Skip("Large benchmark skipped. Use -large to run.")
+	}
+	b.ReportAllocs()
+	for n := 0; n < b.N; n++ {
+		file, _ := os.Open("/tmp/large-file.json")
+		result := []struct{}{}
+		decoder := json.NewDecoder(file)
+		decoder.Decode(&result)
+		file.Close()
+	}
+}
+
+func Benchmark_jsoniter(b *testing.B) {
+	if !*large {
+		b.Skip("Large benchmark skipped. Use -large to run.")
+	}
+	b.ReportAllocs()
+	for n := 0; n < b.N; n++ {
+		file, _ := os.Open("/tmp/large-file.json")
+		iter := jsoniter.Parse(jsoniter.ConfigDefault, file, 4096)
+		for iter.ReadArray() {
+			iter.Skip()
+		}
+		file.Close()
+	}
+}
